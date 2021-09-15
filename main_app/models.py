4a0844e3bcd790a990204eaad624e7b096624ff3
Tanmoy07tech <Tanmoy07tech@users.noreply.github.com>
2021-09-15 13:20:28 +0530

    Created a homepage and added created a student model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django import forms
+# Create your models here.
+class Student(models.Model):
+    first_name=models.CharField(max_length=50,null=False)
+    last_name=models.CharField(max_length=50,null=False)
+    college_id=models.CharField(max_length=50,primary_key=True)
+    email=models.EmailField(max_length=100,null=False,unique=True)
+    password=models.CharField(max_length=12,null=False)
+
+    def __str__(self):
+        return self.college_id
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.core.checks import messages
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from main_app.models import Student
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
+
 
 # Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+def home(request):
+   
+    return render(request,'home.html')
+
+
+def login(request):
+    if request.method=='POST':
+        all_students=Student.objects.all()
+        college_id=request.POST.get('collegeid')
+        
+        raw_pass=request.POST.get('password')
+       
+        
+        verify=False
+        for member in all_students:
+            password_log=check_password(raw_pass,encoded=member.password)
+            
+            if college_id==member.college_id and password_log:    
+                print('succesfully logged')
+                verify=True
+                return HttpResponse('Loggin Successfull')
+                
+            elif college_id==member.college_id and password_log!=member.password:    
+                messages.error(request,'Invalid Password')
+
+                return render(request,'login.html')
+        if verify==False:   
+            messages.error(request,'No registered College Id found.Please register first')
+
+            return render(request,'login.html')   
+    return render(request,'login.html')
+            
+   
+    
+
+def register(request):
+    if request.method=='POST':
+        all_students=Student.objects.all()
+        firstname=request.POST.get('firstname')
+        lastname=request.POST.get('lastname')
+        email=request.POST.get('email')
+        collegeid=request.POST.get('collegeid')
+        password=make_password(request.POST.get('password'))
+        verify=False
+        for member in all_students:
+            if collegeid == member.college_id:
+                
+                messages.error(request,'Student with this College id already exists.Please Login using the same')
+                return render(request,'register.html')
+
+            elif email == member.email:
+                messages.error(request,'Account is already registered with this email.Use some other mail')
+                return render(request,'register.html')
+            else:
+                verify=True
+        if verify==True:
+            s1=Student(first_name=firstname,last_name=lastname,college_id=collegeid,email=email,password=password)
+            s1.save()  
+            print('Created a account')
+            messages.success(request,'Succesfully created an account.Please Login')
+            
+            return render(request,'login.html')
+         
+    
+    return render(request,'register.html')
+
+
+
